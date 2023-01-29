d61ed50a37a1f44157fb55eb6a5f194f85e5f639
Michael Horstmann <git@hrstmnn.de>
2023-01-29 12:08:36 +0100

    fixes and more code

--- src/Arguments_Parser.nim
+++ src/Arguments_Parser.nim
@@ -1,13 +1,33 @@
 ## A parser for command line arguments
 
-import std/[os, tables]
+import std/[os, tables, strutils]
 
-type Arguments* = object
-  input*: seq[string]
-  shortKeys*: seq[string]
-  keys*: seq[string]
-  values*: Table[string, seq[string]]
+type Arguments = object
+  input: seq[string]
+  shortKeys: seq[string]
+  keys: seq[string]
+  values: Table[string, seq[string]]
 
-proc parseInput*(arguments = commandLineParams()): Arguments =
+proc newArguments*(
+    input = newSeq[string](),
+    shortKeys = newSeq[string](),
+    keys = newSeq[string](),
+    values = initTable[string, seq[string]]()
+  ): Arguments = Arguments(
+    input: input,
+    shortKeys: shortKeys,
+    keys: keys,
+    values: values
+  )
+
+proc parseInput*(arguments: seq[string] = commandLineParams()): Arguments =
   ## Parses all command line arguments into one arguments object
+
   echo arguments
+  # for argument in arguments:
+  #   if argument.startsWith("--") or argument.startsWith("-"):
+  #     if argument.contains(":") or argument.contains("="):
+
+  result.keys = @["foo"]
+
+echo parseInput()
--- tests/test1.nim
+++ tests/test1.nim
@@ -5,101 +5,59 @@
 #
 # To run these tests, simply execute `nimble test`.
 
-import unittest, Arguments_Parser
-import std/tables
+import unittest, Arguments_Parser, std/tables
 
 test "one key":
-  check parseInput(@["--foo"]) == Arguments(
-      input: @[],
-      shortKeys: @[],
-      keys: @["foo"],
-      values: initTable[string, seq[string]]()
-    )
+  check parseInput(@["--foo"]) == newArguments(keys = @["foo"])
 
 test "one key with one value":
-  check parseInput(@["--foo:bar"]) == Arguments(
-      input: @[],
-      shortKeys: @[],
-      keys: @["foo"],
-      values: {"foo": @["bar"]}.toTable
+  check parseInput(@["--foo:bar"]) == newArguments(
+      keys = @["foo"],
+      values = {"foo": @["bar"]}.toTable
     )
-  check parseInput(@["--foo=bar"]) == Arguments(
-      input: @[],
-      shortKeys: @[],
-      keys: @["foo"],
-      values: {"foo": @["bar"]}.toTable
+  check parseInput(@["--foo=bar"]) == newArguments(
+      keys = @["foo"],
+      values = {"foo": @["bar"]}.toTable
     )
-  check parseInput(@["--foo", "bar"]) == Arguments(
-      input: @[],
-      shortKeys: @[],
-      keys: @["foo"],
-      values: {"foo": @["bar"]}.toTable
+  check parseInput(@["--foo", "bar"]) == newArguments(
+      keys = @["foo"],
+      values = {"foo": @["bar"]}.toTable
     )
 
 test "one key with two value":
-  check parseInput(@["--foo", "foo", "bar"]) == Arguments(
-      input: @[],
-      shortKeys: @[],
-      keys: @["foo"],
-      values: {"foo": @["foo", "bar"]}.toTable
+  check parseInput(@["--foo", "foo", "bar"]) == newArguments(
+      keys = @["foo"],
+      values = {"foo": @["foo", "bar"]}.toTable
     )
 
 test "one short key":
-  check parseInput(@["-f"]) == Arguments(
-      input: @[],
-      shortKeys: @["f"],
-      keys: @[],
-      values: initTable[string, seq[string]]()
-    )
+  check parseInput(@["-f"]) == newArguments(shortKeys = @["f"])
 
 test "multiple short keys":
-  check parseInput(@["-abcd"]) == Arguments(
-      input: @[],
-      shortKeys: @["a", "b", "c", "d"],
-      keys: @[],
-      values: initTable[string, seq[string]]()
-    )
+  check parseInput(@["-abcd"]) == newArguments(shortKeys = @["a", "b", "c", "d"])
 
 test "one short key with one value":
-  check parseInput(@["-f:bar"]) == Arguments(
-      input: @[],
-      shortKeys: @["f"],
-      keys: @[],
-      values: {"f": @["bar"]}.toTable
+  check parseInput(@["-f:bar"]) == newArguments(
+      shortKeys = @["f"],
+      values = {"f": @["bar"]}.toTable
     )
-  check parseInput(@["-f=bar"]) == Arguments(
-      input: @[],
-      shortKeys: @["f"],
-      keys: @[],
-      values: {"f": @["bar"]}.toTable
+  check parseInput(@["-f=bar"]) == newArguments(
+      shortKeys = @["f"],
+      values = {"f": @["bar"]}.toTable
     )
-  check parseInput(@["-f", "bar"]) == Arguments(
-      input: @[],
-      shortKeys: @["f"],
-      keys: @[],
-      values: {"f": @["bar"]}.toTable
+  check parseInput(@["-f", "bar"]) == newArguments(
+      shortKeys = @["f"],
+      values = {"f": @["bar"]}.toTable
     )
 
 test "one short key with two values":
-  check parseInput(@["-f", "foo", "bar"]) == Arguments(
-      input: @[],
-      shortKeys: @["f"],
-      keys: @[],
-      values: {"f": @["foo", "bar"]}.toTable
+  check parseInput(@["-f", "foo", "bar"]) == newArguments(
+      shortKeys = @["f"],
+      values = {"f": @["foo", "bar"]}.toTable
     )
 
 test "one word input":
-  check parseInput(@["foo"]) == Arguments(
-      input: @["foo"],
-      shortKeys: @[],
-      keys: @[],
-      values: initTable[string, seq[string]]()
-    )
+  check parseInput(@["foo"]) == newArguments(input = @["foo"])
 
 test "two words input":
-  check parseInput(@["foo", "bar"]) == Arguments(
-      input: @["foo", "bar"],
-      shortKeys: @[],
-      keys: @[],
-      values: initTable[string, seq[string]]()
-    )
+  check parseInput(@["foo", "bar"]) == newArguments(input = @["foo", "bar"])
