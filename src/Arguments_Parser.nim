d76da20f0967e91a732c506b0fa72b714d769b7a
Michael Horstmann <git@hrstmnn.de>
2023-01-30 16:30:18 +0100

    removed proc again

--- src/Arguments_Parser.nim
+++ src/Arguments_Parser.nim
@@ -2,11 +2,15 @@
 
 import std/[os, tables, strutils]
 
-type Arguments = object
-  input: seq[string]
-  keys: seq[string]
-  shortKeys: seq[string]
-  values: Table[string, seq[string]]
+type
+  Arguments = object
+    input: seq[string]
+    keys: seq[string]
+    shortKeys: seq[string]
+    values: Table[string, seq[string]]
+
+  Argument = enum
+    input, long, short
 
 proc newArguments*(
     input = newSeq[string](),
@@ -20,41 +24,53 @@ proc newArguments*(
     values: values
   )
 
-proc addSplitArgument(argument: string, sep: string, arguments: var Arguments) =
-  let splitArgument = argument.split(sep)
-  arguments.keys.add(splitArgument[0])
-  arguments.values[splitArgument[0]].add(splitArgument[1])
-
 proc parseInput*(arguments: seq[string] = commandLineParams()): Arguments =
   ## Parses all command line arguments into one arguments object
-
-  type Argument = enum
-    input, long, short
 
   var argumentType = input
 
   for argument in arguments:
+    echo argument
+
+    var firstElement = false
+
     if argument.startsWith("--"):
       argumentType = long
+      firstElement = true
     elif argument.startsWith("-"):
       argumentType = short
+      firstElement = true
 
     case argumentType
       of input:
         result.input.add(argument)
       of long:
         if argument.contains(":"):
-          addSplitArgument(argument, ":", result)
+          let splitArgument = argument.split(":")
+          result.keys.add(splitArgument[0])
+          result.values[splitArgument[0]].add(splitArgument[1])
         elif argument.contains("="):
-          addSplitArgument(argument, "=", result)
+          let splitArgument = argument.split("=")
+          result.keys.add(splitArgument[0])
+          result.values[splitArgument[0]].add(splitArgument[1])
         else:
-          result.values[result.keys[^1]].add(argument)
+          if firstElement:
+            result.keys.add(argument)
+          else:
+            result.values[result.keys[^1]].add(argument)
       of short:
         if argument.contains(":"):
-          addSplitArgument(argument, ":", result)
+          let splitArgument = argument.split(":")
+          result.shortKeys.add(splitArgument[0])
+          result.values[splitArgument[0]].add(splitArgument[1])
         elif argument.contains("="):
-          addSplitArgument(argument, "=", result)
+          let splitArgument = argument.split("=")
+          result.shortKeys.add(splitArgument[0])
+          result.values[splitArgument[0]].add(splitArgument[1])
         else:
-          result.values[result.shortKeys[^1]].add(argument)
+          if firstElement:
+            result.shortKeys.add(argument)
+          else:
+            result.values[result.shortKeys[^1]].add(argument)
 
 echo "Arguments: ", parseInput()
