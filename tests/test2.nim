831828c93ff40b26b4f8173b2ff9edffad6f2313
Michael Horstmann <git@hrstmnn.de>
2023-01-31 13:47:17 +0100

    more tests

--- tests/test2.nim
+++ tests/test2.nim
@@ -6,3 +6,16 @@ test "two words input and one key":
     keys = @["foo"],
     values = {"foo": newSeq[string]()}.toTable
   )
+
+test "empty keys":
+  check parseInput(@["-", "--"]) == newArguments()
+  check parseInput(@["foo", "bar", "-", "--"]) == newArguments(input = @["foo", "bar"])
+  check parseInput(@["-", "foo", "--", "bar"]) == newArguments(input = @["foo", "bar"])
+  check parseInput(@["-", "--", "foo", "bar"]) == newArguments(input = @["foo", "bar"])
+
+test "write output to file":
+  check parseInput(@["main.c", "-o", "main"]) == newArguments(
+    input = @["main.c"],
+    shortKeys = @["o"],
+    values = {"o": @["main"]}.toTable
+  )
