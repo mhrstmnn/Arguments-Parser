0efd2c064ceaa4b8d3dce902a59c35ed276eb150
Michael Horstmann <git@hrstmnn.de>
2023-02-03 01:29:13 +0100

    more tests

--- tests/test2.nim
+++ tests/test2.nim
@@ -19,3 +19,37 @@ test "write output to file":
     shortKeys = @["o"],
     values = {"o": @["main"]}.toTable
   )
+
+test "one key with one value in quotes":
+  check parseInput(@["--foo:\"foo bar\""]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+  check parseInput(@["--foo:'foo bar'"]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+  check parseInput(@["--foo=\"foo bar\""]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+  check parseInput(@["--foo='foo bar'"]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+  check parseInput(@["--foo", "\"foo bar\""]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+  check parseInput(@["--foo", "'foo bar'"]) == newArguments(
+    keys = @["foo"],
+    values = {"foo": @["foo bar"]}.toTable
+  )
+
+test "two words input in quotes":
+  check parseInput(@["\"foo bar\""]) == newArguments(
+    input = @["foo bar"],
+  )
+  check parseInput(@["'foo bar'"]) == newArguments(
+    input = @["foo bar"],
+  )
